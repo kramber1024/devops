20dd8c1c54729790c8b6db708cc5e1a5fc161da5
kramber1024 <kramber268@gmail.com>
2024-11-23 15:31:54 +0300

    feat: Add endpoint for getting all users

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,13 +1,35 @@
-from fastapi import APIRouter, status
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app import crud
+from app.api import schemes
+from app.core.database import database
 
 router: APIRouter = APIRouter(
     prefix="/users",
 )
 
 
-@router.get("/")
-def get_users() -> JSONResponse:
+@router.get(
+    "",
+    summary="Получить информацию o всех пользователях",
+    description="Boзвpaщaeт информацию o всех пользователях.",
+    responses={
+        status.HTTP_200_OK: {},
+    },
+)
+async def get_users(
+    session: Annotated[
+        AsyncSession,
+        Depends(database.get_scoped_session),
+    ],
+) -> JSONResponse:
     return JSONResponse(
-        content={"message": "GET users"}, status_code=status.HTTP_200_OK
+        content=schemes.UserList.from_models(
+            await crud.get_users_all(session=session),
+        ).model_dump(mode="json"),
+        status_code=status.HTTP_200_OK,
     )
